c9bce2ce25df42b6c3f0fe54d5f3883f44d75919
Chiran Jeewantha <chiran@thesanmark.com>
2023-02-07 16:06:26 +0530

    .

--- auth-extension/src/iam/authentication/authentication.controller.ts
+++ auth-extension/src/iam/authentication/authentication.controller.ts
@@ -13,17 +13,22 @@ export class AuthenticationController {
         return this.authService.signUp(signUpDto);
     }
 
+    // @HttpCode(HttpStatus.OK)
+    // @Post('sign-in')
+    // async signIn(
+    //     @Res({ passthrough: true}) response: Response,
+    //     @Body() signInDto: SignInDto,
+    //     ) {
+    //         const accessToken = await this.authService.signIn(signInDto);
+    //         response.cookie('accessToken', accessToken, {
+    //             secure: true,
+    //             httpOnly: true,
+    //             sameSite: true,
+    //         });
+    // }
     @HttpCode(HttpStatus.OK)
     @Post('sign-in')
-    async signIn(
-        @Res({ passthrough: true}) response: Response,
-        @Body() signInDto: SignInDto,
-        ) {
-            const accessToken = await this.authService.signIn(signInDto);
-            response.cookie('accessToken', accessToken, {
-                secure: true,
-                httpOnly: true,
-                sameSite: true,
-            });
+    signIn(@Body() signInDto: SignInDto) {
+        return this.authService.signIn(signInDto);
     }
 }
--- auth-extension/src/iam/authentication/guards/access-token/access-token.guard.ts
+++ auth-extension/src/iam/authentication/guards/access-token/access-token.guard.ts
@@ -25,7 +25,6 @@ export class AccessTokenGuard implements CanActivate {
         this.jwtConfiguration,
       );
       request[REQUEST_USER_KEY] = payload;
-      console.log(payload);
     } catch {
       throw new UnauthorizedException();
     }
