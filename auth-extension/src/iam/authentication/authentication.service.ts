0b9cfe3e6e8530fe50f38e12d555dc7412e2a186
Chiran Jeewantha <chiran@thesanmark.com>
2023-02-07 09:53:38 +0530

    .

--- auth-extension/src/iam/authentication/authentication.service.ts
+++ auth-extension/src/iam/authentication/authentication.service.ts
@@ -1,8 +1,9 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { HashingService } from '../hashing/hashing.service';
+import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 
 @Injectable()
@@ -26,5 +27,23 @@ export class AuthenticationService {
             }
             throw err;
         }
+    }
+
+    async signIn(signInDto: SignInDto) {
+        const user = await this.usersRepository.findOneBy({
+            email: signInDto.email,
+        });
+        if (!user) {
+            throw new UnauthorizedException('User does not exists');
+        }
+        const IsEmail = await this.hashingService.compare(
+            signInDto.password,
+            user.password
+        );
+        if (!IsEmail) {
+            throw new UnauthorizedException('Password does not match');
+        }
+        
+        return true;
     }
 }
